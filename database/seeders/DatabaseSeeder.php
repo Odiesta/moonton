81853348998fc61c9cda35c258a915e4b7bd4dd6
Odiesta Shandikarona <shandikaronamade@gmail.com>
2025-09-06 15:08:04 +0800

    feat: migration and seeders files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleTableSeeder::class,
             UserTableSeeder::class,
+            SubscriptionPlanTableSeeder::class,
+            MovieTableSeeder::class,
             // Add other seeders here as needed
         ]);
     }
---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Movie;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $movies = [
+            [
+                'name' => 'The Shawshank Redemption',
+                'slug' => 'the-shawshank-redemption',
+                'category' => 'Drama',
+                'video_url' => 'https://www.youtube.com/watch?v=6hB3S9bIaco',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWM',
+                'rating' => 9.3,
+                'is_featured' => true
+            ],
+            [
+                'name' => 'The Godfather',
+                'slug' => 'the-godfather',
+                'category' => 'Crime',
+                'video_url' => 'https://www.youtube.com/watch?v=sY1S34973zA',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk,',
+                'rating' => 9.2,
+                'is_featured' => false
+            ],
+            [
+                'name' => 'The Dark Knight',
+                'slug' => 'the-dark-knight',
+                'category' => 'Action',
+                'video_url' => 'https://www.youtube.com/watch?v=EXeTwQWrcwY',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@',
+                'rating' => 9.0,
+                'is_featured' => false
+            ]
+        ];
+
+        Movie::insert($movies);
+    }
+}
